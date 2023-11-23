f8bbb10032cc2655ed484b9ad3efd6b1cf792f52
fernanda souza <135151346+devfernandasouza@users.noreply.github.com>
2023-11-23 10:15:54 -0300

    Create prog1.c

---
+++ prog1.c
@@ -0,0 +1,22 @@
+// comentario de uma linha
+
+/*
+Exemplo de um 
+comemtario 
+com multliplas
+linhas
+*/
+
+/*
+*UFMT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Implementacao da funcao principal
+int main(){
+    printf("Olá mundo!\n");
+    return 1;
+}
