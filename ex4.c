43a6aa69ef9cef6ffabdb19ffae4865b730417ab
fernanda souza <135151346+devfernandasouza@users.noreply.github.com>
2023-11-26 18:52:19 -0300

    beecrowd | 1008 Salário

---
+++ ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int func, horas;
+    float valor,salario; 
+
+    scanf ("%d" "%d" , &func, &horas);
+    scanf("%f", &valor);
+
+    salario = horas * valor;
+
+    printf("NUMBER = %d\n",func);
+    printf("SALARY = U$ %.2f\n", salario);
+    return 0;
+
+
+
+}
