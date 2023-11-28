dec1dbd3d8f8337efef3929d9aac553d69d3a73a
fernanda souza <135151346+devfernandasouza@users.noreply.github.com>
2023-11-28 15:16:45 -0300

    Add files via upload

---
+++ prog6.c
@@ -0,0 +1,16 @@
+/***
+ * UFMT -Ciencia da Computacao
+ * Estrutura de Dados
+ * Aluna = Fernanda Lima de Souza
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int contador;
+    for( contador = 0; contador <= 5; contador +1 ){
+        printf("Contando %d...\n, contador");
+    }
+    return 1;
+}
