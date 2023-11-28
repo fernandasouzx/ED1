90053f5bd8daedc4cc07de754254fbf7dd14b670
fernanda souza <135151346+devfernandasouza@users.noreply.github.com>
2023-11-28 15:18:06 -0300

    Add files via upload

---
+++ prog10.c
@@ -0,0 +1,24 @@
+/***
+ * UFMT -Ciencia da Computacao
+ * Estrutura de Dados
+ * Aluna = Fernanda Lima de Souza
+ * Exercicio 10 -Exemplo de operadores diversos
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x, y, z;
+    
+    x= 7;
+    y= 3;
+
+    z= x ^ y;
+    
+    z= z<<2;
+
+    printf("z = %d \n", z);
+
+    return 1;
+}
